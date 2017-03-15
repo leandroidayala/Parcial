1b91afc90da8c9c5f1b77801a607a7650cd069a7
aula7 <pc703@gmail.com>
2017-03-15 13:52:23 -0500

    Avance 3 Parcial

--- app/src/main/java/com/example/aula7/parcial/SecondActivity.java
+++ app/src/main/java/com/example/aula7/parcial/SecondActivity.java
@@ -2,12 +2,21 @@ package com.example.aula7.parcial;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second2);
+        setContentView(R.layout.activity_second);
+        showToolbar(getResources().getString(R.string.str_title_toolbar),true);
+    }
+
+    public void showToolbar(String title, boolean upButton) {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
     }
 }
